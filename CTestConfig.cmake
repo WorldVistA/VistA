38e09b3fcc1aabdafd04684ac85ef8c68d41aa6d
Joseph Snyder <joe.snyder@kitware.com>
2011-10-03 15:27:43 -0400

    Update the CTestConfig.cmake to submit to the Open Source EHR project instead of the OSEHR project
    Change-Id: I5b1279d687a13ca364bdd74818022db29966f067

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -21,10 +21,11 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "OSEHR")
+set(CTEST_PROJECT_NAME "Open Source EHR")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "code.osehra.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=OSEHR")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Open+Source+EHR")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
