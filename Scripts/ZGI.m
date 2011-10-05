0b37448b8ba3289b717f081e5b20089b7b4b9267
Brad King <brad.king@kitware.com>
2011-10-05 13:47:13 -0400

    ZGI: Abstract M-vendor device APIs
    Automatically configure Cache or GT.M device interaction behind a common
    internal API.  We cannot use the Kernel package API because this routine
    must work independently and before the Kernel is loaded.
    
    Original-Change-Id: I40df737ccf52286e6661d4af2eb414f0847b7be1

--- Scripts/ZGI.m
+++ Scripts/ZGI.m
@@ -1,4 +1,4 @@
-ZGI ; Read Globals from ZWR format into InterSystems Cache
+ZGI ; Read Globals from ZWR format
  ;---------------------------------------------------------------------------
  ; Copyright 2011 The Open Source Electronic Health Record Agent
  ;
@@ -14,33 +14,58 @@ ZGI ; Read Globals from ZWR format into InterSystems Cache
  ; See the License for the specific language governing permissions and
  ; limitations under the License.
  ;---------------------------------------------------------------------------
- N  W "ZWR Global Input",! D IN^%IS Q:POP  W !
- I '$$LOAD(IO) Q 0
+ N  D CONFIG
+ W "ZWR Global Input",! X CONFIG("ASKIO") Q:IO=""  W !
+ I '$$LOAD(IO) Q
  U $P W "Loaded "_IO,!
- Q 1
+ Q
 LOAD(IO) ; Read Globals from IO device in ZWR format
+ N CONFIG D CONFIG
  N Q,S S Q=1
- C IO O IO:("R"):0 I '$T W "Failed to open: "_IO,! Q 0
+ C IO X CONFIG("OPENIO") I '$T W "Failed to open: "_IO,! Q 0
  I '($$READLINE(.H1)&$$READLINE(.H2)&(H2["ZWR")) U $P W "Not a ZWR: "_IO,! S Q=0 G QUIT
  F  Q:'$$READLINE(.S)  S @S
  G QUIT
 LIST(IO,DIR) ; Read list from IO, load Globals from each DIR<entry> device
+ N CONFIG D CONFIG
  N Q,ZWR S Q=1
- C IO O IO:("R"):0 I '$T W "Failed to open: "_IO,! Q 0
+ C IO X CONFIG("OPENIO") I '$T W "Failed to open: "_IO,! Q 0
  F  Q:'$$READLINE(.ZWR)  U $P W DIR_ZWR,! I '$$LOAD(DIR_ZWR) S Q=0
  G QUIT
+ ;---------------------------------------------------------------------------
+ ; Private implementation entry points below
+ ;
 READLINE(LINE)
- N $ES,$ET,EOF S $ET="G ETRAP",EOF=0
+ N $ES,$ET,EOF S EOF=0 X CONFIG("ETRAP")
  D READLN(.LINE)
- Q '(($ZEOF=-1)!EOF)
+ Q '($ZEOF!EOF)
 READLN(LINE)
  S LINE=""
  U IO R LINE
  Q
 ETRAP
- I $ZE["ENDOFFILE" S EOF=-1
+ I @(CONFIG("ENDOFFILE")) S EOF=-1
  S $EC=""
  Q
 QUIT
  C IO
  Q Q
+CONFIG
+ I $D(CONFIG) Q
+ I $ZV["Cache" D  Q
+ . S CONFIG("ASKIO")="D IN^%IS I POP S IO="""""
+ . S CONFIG("OPENIO")="O IO:(""R""):0"
+ . S CONFIG("ETRAP")="S $ET=""G ETRAP"""
+ . S CONFIG("ENDOFFILE")="$ZE[""ENDOFFILE"""
+ I $ZV["GT.M" D  Q
+ . S CONFIG("ASKIO")="D ASKIO"
+ . S CONFIG("OPENIO")="O IO:(readonly):0"
+ . S CONFIG("ETRAP")="U IO:(exception=""G ETRAP"")"
+ . S CONFIG("ENDOFFILE")="$ZS[""IOEOF"""
+ W "ZGI does not support "_$ZV,!
+ Q
+ASKIO
+ R "Device: ",IO,!
+ I IO="" G ASKIO
+ I IO="^" S IO=""
+ Q
