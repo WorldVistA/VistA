bd22515b3b1f234d9e917d7680d4eb123a18fe05
Joseph Snyder <joe.snyder@kitware.com>
2011-10-13 18:24:29 -0400

    Create a DashboardScripts directory to hold machine build scripts.
    This commit creates a DashboardScripts directory for the storage
    of scripts used to drive automated ctest build scripts. There will
    be a build script for each machine running nightly and and other
    continuous testing. The scripts should all use the vista_common.cmake
    script.
    
    Change-Id: I9ee808f5b859f96fac0337f2d265e6fa47e1f97d

---
+++ DashboardScripts/kitware/joseph-desktop.sh
@@ -0,0 +1,24 @@
+#---------------------------------------------------------------------------
+# Copyright 2011 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+cd /home/joseph/Downloads/Testbin
+GTMPROFILE=/opt/lsb-gtm/V5.4-000A_x86_64/gtmprofile
+VISTA_GLOBALS_DIR=/home/joseph/vista
+VISTA_ROUTINES_DIR=/home/joseph/vista/r
+. $GTMPROFILE
+gtmgbldir="$VISTA_GLOBALS_DIR/database"
+gtmroutines="${gtmroutines} $VISTA_ROUTINE_DIR"
+export GTMPROFILE VISTA_GLOBALS_DIR VISTA_ROUTINES_DIR gtmgbldir gtmroutines
+ctest -S DashboardScripts/kitware/joseph-desktop.cmake -VV
