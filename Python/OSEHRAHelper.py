30005ddaf05244434bc2e219db58adcdbe026932
Joseph Snyder <joe.snyder@kitware.com>
2012-02-09 14:36:23 -0500

    Create and Utilize OSEHRAHelper Python module
    Create a python module called OSEHRAHelper that is initialized with data
    about the instance you will connect to. The Module  will automatically
    determine the type and connection method.
    
    Based on the system it will use the telnetlib to connect to a Win32-Cache
    instance, and use pexpect to connect to the two Linux types.  With this module,
    the scripts for testing are more generic, utilizing an abstract script to
    connect to all three MUMPS instances. It will also store information which
    can be used for code that needs a particular platform to run.
    
    The information needed during initialization is the path to
    a log file(required) and optionally: an Instance name, a namespace and
    a location.  The optional arguments have defaults which will be used if
    no variable is provided.  'Instance' defaults to 'CACHE', namespace to
    'VISTA', and location to '127.0.0.1'.
    
    Create two new options in the CMakelists.txt file to customize the XINDEX
    scripts.  One option for needing to log into the instance called
    "USE_LOGIN", the second for changing namespace at the beginning of the
    script.  The USE_LOGIN will ask the user for the username and password to
    supply to the instance when necessary.
    
    Create a generic XINDEX python script that the testing will be called from the
    PythonRoutine.cmake.in after configuration that takes 7 arguments.  The
    arguments need to be in the following order: LogFilePath,
    ListofRoutines (automatically generated), INSTANCE, NAMESPACE, USERNAME,
    PASSWORD, VISTA_SOURCE_DIR.  The XINDEX python script should use both the
    system type and the VistA.type to insert prompts that are specific to
    particular instances.
    
    Change-Id: I2de1dd75a7f0d353d77512c44d36ed9eddb4f015

---
+++ Python/OSEHRAHelper.py
@@ -0,0 +1,119 @@
+r"""Class to Connect to a VistA EHR automatically."""
+#---------------------------------------------------------------------------
+# Copyright 2012 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+import sys
+import os
+import telnetlib
+try:
+  import pexpect
+  no_pexpect = None
+except ImportError, no_pexpect:
+  pass
+
+#---------------------------------------------------------------------------
+#Initial Global Variables to use over the course of connecting
+
+connection=False
+log =False
+
+#---------------------------------------------------------------------------
+class PROMPT(object):
+  """Wait for a VISTA> prompt in current namespace."""
+
+class ConnectMUMPS(object):
+
+  def ZN(self,namespace):
+    self.wait('>')
+    self.write('ZN "' + namespace + '"')
+    self.wait(namespace + '>')
+    self.namespace = namespace
+
+  def login(self,username,password):
+    self.wait('Username:')
+    self.write(username)
+    self.wait('Password')
+    self.write(password)
+
+class ConnectWinCache(ConnectMUMPS):
+  def __init__(self,logfile,instance,namespace='VISTA',location='127.0.0.1'):
+    global connection,log
+    super(ConnectMUMPS,self).__init__()
+    connection=telnetlib.Telnet(location,23)
+    log=file(logfile,'w')
+    self.type='cache'
+
+  def write(self,command ):
+    global connection
+    connection.write(command + '\r')
+
+  def wait(self,command ):
+    global connection
+    if command is PROMPT:
+      command = self.namespace + '>'
+    log.write(connection.read_until(command))
+
+class ConnectLinuxCache(ConnectMUMPS):
+  def __init__(self,logfile,instance,namespace='VISTA',location='127.0.0.1'):
+    global connection,log
+    super(ConnectMUMPS,self).__init__()
+    connection=pexpect.spawn('ccontrol session ' + instance + ' -U ' + namespace,timeout=None)
+    connection.logfile_read = file(logfile,'w')
+    self.type='cache'
+
+  def write(self,command ):
+    connection.send(command + '\r')
+
+  def wait(self,command ):
+    if command is PROMPT:
+      command = self.namespace + '>'
+    connection.expect(command)
+
+class ConnectLinuxGTM(ConnectMUMPS):
+  def __init__(self,logfile,instance,namespace='GTM',location='127.0.0.1'):
+    global connection,log
+    super(ConnectMUMPS,self).__init__()
+    connection=pexpect.spawn('gtm', timeout=None)
+    connection.logfile_read = file(logfile,'w')
+    self.type='GTM'
+
+  def write(self,command ):
+    connection.send(command + '\r')
+
+  def wait(self,command ):
+    if command is PROMPT:
+      command = self.namespace + '>'
+    connection.expect(command)
+
+def ConnectToMUMPS(logfile,instance='CACHE',namespace='VISTA',location='127.0.0.1'):
+
+    global connection
+    #self.namespace = namespace
+    #self.location = location
+    #print "You are using " + sys.platform
+    if sys.platform == 'win32':
+      return ConnectWinCache(logfile,instance,namespace,location)
+    elif sys.platform == 'linux2':
+      if no_pexpect:
+        raise no_pexpect
+      try:
+        return ConnectLinuxCache(logfile,instance,namespace,location)
+      except pexpect.ExceptionPexpect, no_cache:
+        pass
+      try:
+        return ConnectLinuxGTM(logfile,instance,namespace,location)
+      except pexpect.ExceptionPexpect, no_gtm:
+         if (no_cache and no_gtm):
+           raise "Cannot find a MUMPS instance"
---
+++ XINDEXScript.py
@@ -0,0 +1,41 @@
+import sys
+sys.path = [sys.argv[7] + '/Python'] + sys.path
+
+from OSEHRAHelper import ConnectToMUMPS,PROMPT
+
+VistA=ConnectToMUMPS(sys.argv[1],sys.argv[3],sys.argv[4])
+if (sys.argv[5] and sys.argv[6]):
+  VistA.login(sys.argv[5],sys.argv[6])
+if VistA.type=='cache':
+  try:
+    VistA.ZN(sys.argv[4])
+  except IndexError,no_namechange:
+    pass
+VistA.write('D ^XINDEX')
+if VistA.type == 'cache':
+  VistA.wait('No =>')
+  VistA.write('No')
+arglist = sys.argv[2].split(',')
+for routine in arglist:
+  VistA.wait('Routine:')
+  VistA.write(routine)
+VistA.wait('Routine:')
+VistA.write('')
+VistA.wait('Select BUILD NAME:')
+VistA.write('')
+VistA.wait('Select PACKAGE NAME:')
+VistA.write('')
+VistA.wait('warnings?')
+VistA.write('No')
+VistA.wait('routines?')
+VistA.write('NO')
+VistA.wait('DEVICE:')
+VistA.write(';;9999')
+if sys.platform == 'win32':
+  VistA.wait('Right Margin:')
+  VistA.write('')
+VistA.write('')
+VistA.wait('continue:')
+VistA.write('')
+VistA.wait('--- END ---')
+VistA.write('h')
