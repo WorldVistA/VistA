f8cae18da615d623bcd567587e57f136fd06fd03
Joseph Snyder <joe.snyder@kitware.com>
2012-04-11 13:22:51 -0400

    Add CTest capability and information
    Change the CMakeLists.txt to include CTest, which allows the suite to
    submit testing results to a dashboard.
    Add a CTestConfig.cmake file with the information for the OSEHRA Software
    Quality Dashboard, where CTest will submit to.
    
    Original-Change-Id: Ie5cdfd3e709b5480b6ee5cc1c8f6eba61d1d1cd1

---
+++ CTestConfig.cmake
@@ -0,0 +1,28 @@
+#---------------------------------------------------------------------------
+# Copyright 2011 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "Open Source EHR")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "code.osehra.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Open+Source+EHR")
+set(CTEST_DROP_SITE_CDASH TRUE)
