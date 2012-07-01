2d0d92ec57cb2609ca68b51301dff99096df2777
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-01 17:00:37 -0400

    ENH: Improving gtmroutines environment variable.

--- Scripts/installVistAinUserAccount.sh
+++ Scripts/installVistAinUserAccount.sh
@@ -4,8 +4,9 @@ VistADir=$HOME/VistA
 gtm_dist=/usr/lib/fis-gtm/V5.5-000_x86_64
 mkdir -p $VistADir
 mkdir -p $VistADir/r
+mkdir -p $VistADir/o
 source $gtm_dist/gtmprofile
 export gtmgbldir=$VistADir/database
-export gtmroutines=$VistADir/r
+export gtmroutines="$VistADir/o($VistADir/r) $gtm_dist/libgtmutil.so"
 $gtm_dist/mumps -r GDE
 
