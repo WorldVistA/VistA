3d7a6388e34e032b2a3b833ca667e2bf78efad31
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-01 19:46:09 -0400

    BUG: It was missing definition of VistADir.

--- Scripts/importingVistAFOIAintoGTM.sh
+++ Scripts/importingVistAFOIAintoGTM.sh
@@ -3,6 +3,7 @@
 # More details at
 # http://www.osehra.org/wiki/importing-osehra-code-base-gtm
 #
+VistADir=$HOME/VistA
 gtm_dist=/usr/lib/fis-gtm/V5.5-000_x86_64
 source $gtm_dist/gtmprofile
 export gtmgbldir=$VistADir/database
