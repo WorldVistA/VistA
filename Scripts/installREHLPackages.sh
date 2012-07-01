8a67c27206bb6e6d93f55b3f3cd43177fe34c33c
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-01 18:33:00 -0400

    ENH: Packages needed in RHEL 6.
    These packages are needed by some of the subsequent software that
    a VistA installation and testing environment rely on.

---
+++ Scripts/installREHLPackages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum update
+yum install libicu
+yum install libicu-devel
+yum install git
+yum install gcc
+yum install gcc-c++
+yum install make
+yum install ncurses
+yum install ncurses-devel
