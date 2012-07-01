8235dd07340f2bcf239a6954d652e9f6df650c06
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-01 19:42:43 -0400

    ENH: Script to import VistA into GTM.
    Based on instructions from:
    http://www.osehra.org/wiki/importing-osehra-code-base-gtm

---
+++ Scripts/importingVistAFOIAintoGTM.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# More details at
+# http://www.osehra.org/wiki/importing-osehra-code-base-gtm
+#
+gtm_dist=/usr/lib/fis-gtm/V5.5-000_x86_64
+source $gtm_dist/gtmprofile
+export gtmgbldir=$VistADir/database
+export gtmroutines="$VistADir/o($VistADir/r) $gtm_dist/libgtmutil.so"
+$gtm_dist/gtm
+#
+# DO ^%RI
+#
