af7e3b27ab84d71297acdd08faf6d82891822821
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-02 13:45:30 -0400

    ENH: Remove two top lines from KIDS build.

--- Scripts/installMUnit.sh
+++ Scripts/installMUnit.sh
@@ -8,3 +8,7 @@ mkdir -p $HOME/OSEHRA
 cd $HOME/OSEHRA
 git clone git://github.com/OSEHR/M-Tools.git
 cd M-Tools
+cd "Utilities XT_7.3_81 not yet released"
+pwd
+# Remove the top two lines
+sed -i.bak -e '1,2 d' XT_7-3_81_TESTVER9.KID
