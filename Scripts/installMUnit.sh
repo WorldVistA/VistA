5dd8933c79ed37b6d895991533bc155f6f7d75be
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-02 13:36:02 -0400

    ENH: Script to install M-Unit from Git.
    Clone a git repository in to the users' machine.

---
+++ Scripts/installMUnit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# More details at
+# http://www.osehra.org/wiki/importing-osehra-code-base-gtm
+#
+cd $HOME
+mkdir -p $HOME/OSEHRA
+cd $HOME/OSEHRA
+git clone git://github.com/OSEHR/M-Tools.git
+cd M-Tools
