b4c06c9dd00a6266ebad4e59d6a0d8f8d4e552ab
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-03 07:38:17 -0400

    ENH: Script to add lines to .bashrc file.
    In particular to source the environment variables and to modify
    the PATH in order to find the GT.M executables and the installed
    CMake executables.

---
+++ Scripts/installBashConfiguration.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+#  Add configuration lines to the .bashrc file.
+#
+echo "" >> $HOME/.bashrc
+echo '# OSEHRA User specific aliases and functions' >> $HOME/.bashrc
+echo 'source $HOME/OSEHRA/VistA-installation-scripts/Scripts/setupEnvironmentVariables.sh' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$gtm_dist:$HOME/local/bin' >> $HOME/.bashrc
