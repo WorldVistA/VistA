b1d98b0a9f4d4e8968179fa79ee8946156d38633
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-03 14:55:40 -0400

    ENH: Opening port for CPRS, and fixing user.

--- Scripts/configureCPRS.sh
+++ Scripts/configureCPRS.sh
@@ -5,3 +5,8 @@
 #  You MUST use "sudo" to execute this script.
 #
 sudo cp $HOME/OSEHRA/VistA-installation-scripts/Scripts/cprs-vista-xinetd /etc/xinetd.d/
+
+#
+# Open the port where the RPC broker will be listening.
+#
+sudo iptables -A INPUT -p tcp --dport 9430 -j ACCEPT
