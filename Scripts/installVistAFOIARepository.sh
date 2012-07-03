a462fd19fcd969404de6d3b5294d795af17ee992
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-03 14:00:16 -0400

    ENH: Edit the ZTLOAD1.m to fix Lock problem.

--- Scripts/installVistAFOIARepository.sh
+++ Scripts/installVistAFOIARepository.sh
@@ -7,3 +7,9 @@ export DashboardsDir=$HOME/OSEHRA/Dashboards
 mkdir -p $DashboardsDir
 cd $DashboardsDir
 git clone git://code.osehra.org/VistA-FOIA.git
+#
+# switch position of two lines to solve problem with Locks.
+#
+sed i.bak -e '82 i\
+ TCOMMIT  ;' $HOME/OSEHRA/Dashboards/VistA-FOIA/Packages/Kernel/Routines/ZTLOAD1.m
+sed i.bak -e '84 d' $HOME/OSEHRA/Dashboards/VistA-FOIA/Packages/Kernel/Routines/ZTLOAD1.m
