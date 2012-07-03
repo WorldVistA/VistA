fcb12cfbcdc949dd5905cc0c519dfe398a3fc248
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-03 13:49:03 -0400

    ENH: Added documentation about using sudo.

--- Scripts/configureCPRS.sh
+++ Scripts/configureCPRS.sh
@@ -2,4 +2,6 @@
 #
 #  Configuring network services for CPRS.
 #
+#  You MUST use "sudo" to execute this script.
+#
 sudo cp $HOME/OSEHRA/VistA-installation-scripts/Scripts/cprs-vista-xinetd /etc/xinetd.d/
