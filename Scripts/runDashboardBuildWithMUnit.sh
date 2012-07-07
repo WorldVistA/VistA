e06174e6e5c17e75dc10d08b7ee7b6fc2876cdc7
Luis Ibanez <luis.ibanez@kitware.com>
2012-07-07 12:08:37 -0400

    ENH: Now getting hostname from /proc/sys/kernel.
    Invoking the hostname command didn't work. Now we get the hostname
    by doing: cat /proc/sys/kernel/hostname, which should not rely on
    the user being logged in.

--- Scripts/runDashboardBuildWithMUnit.sh
+++ Scripts/runDashboardBuildWithMUnit.sh
@@ -3,7 +3,7 @@ source $HOME/OSEHRA/VistA-installation-scripts/Scripts/setupEnvironmentVariables
 export VistAGlobalsDir=$VistADir/g
 export DashboardsDir=$HOME/OSEHRA/Dashboards
 export PATH=$PATH:$gtm_dist:$HOME/local/bin
-export HOST_NAME=`hostname`
+export HOST_NAME=`cat /proc/sys/kernel/hostname`
 mkdir -p $DashboardsDir/Logs
 $HOME/local/bin/ctest -j 4 \
 -S $HOME/OSEHRA/VistA-installation-scripts/Scripts/runDashboardBuildWithMUnit.cmake \
