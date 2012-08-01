65ce6e4c361960ecc8ce16eda6b1af4a77b4c6fa
Luis Ibanez <luis.ibanez@kitware.com>
2012-08-01 14:43:49 -0400

    ENH: Split setup for bashrc.
    Split actions related to CMake path, from actions related to GTM
    paths.

--- Scripts/installBashConfiguration.sh
+++ Scripts/installBashConfiguration.sh
@@ -4,5 +4,4 @@
 #
 echo "" >> $HOME/.bashrc
 echo '# OSEHRA User specific aliases and functions' >> $HOME/.bashrc
-echo 'source $HOME/OSEHRA/VistA-installation-scripts/Scripts/setupEnvironmentVariables.sh' >> $HOME/.bashrc
-echo 'export PATH=$PATH:$gtm_dist:$HOME/local/bin' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/local/bin' >> $HOME/.bashrc
