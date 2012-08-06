a4f70331803fc58e72bc4629cf2db6650a7e37f1
Luis Ibanez <luis.ibanez@kitware.com>
2012-08-06 15:59:02 -0400

    Change git address for read-only

--- Scripts/installOSEHRATesting.sh
+++ Scripts/installOSEHRATesting.sh
@@ -6,7 +6,7 @@
 export DashboardsDir=$HOME/OSEHRA/Dashboards
 mkdir -p $DashboardsDir
 cd $DashboardsDir
-git clone git@github.com:OSEHR/OSEHRA-Automated-Testing.git
+git clone git://github.com/OSEHR/OSEHRA-Automated-Testing.git
 git checkout --track UseCaseTesting
 ln -s $DashboardsDir/OSEHRA-Automated-Testing/DashboardScripts/vista_common.cmake $HOME/OSEHRA/VistA-installation-scripts/Scripts
 
