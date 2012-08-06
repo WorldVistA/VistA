0f474e6414dfaed05966dbf42f9954a1acbf7c1c
Luis Ibanez <luis.ibanez@kitware.com>
2012-08-06 15:47:45 -0400

    ENH: Now cloning from the OSEHR fork.
    The fork in the OSEHR sandbox has upstream testing scripts that are not
    available yet in the OSEHRA git repository. We also explicitly checkout
    the UseCaseTesting branch.

--- Scripts/installOSEHRATesting.sh
+++ Scripts/installOSEHRATesting.sh
@@ -6,6 +6,7 @@
 export DashboardsDir=$HOME/OSEHRA/Dashboards
 mkdir -p $DashboardsDir
 cd $DashboardsDir
-git clone git://github.com/OSEHRA/OSEHRA-Automated-Testing.git
+git clone git@github.com:OSEHR/OSEHRA-Automated-Testing.git
+git checkout --track UseCaseTesting
 ln -s $DashboardsDir/OSEHRA-Automated-Testing/DashboardScripts/vista_common.cmake $HOME/OSEHRA/VistA-installation-scripts/Scripts
 
