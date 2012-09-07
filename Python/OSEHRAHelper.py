30ba2e04ebf61fc8077450c1ff36b2311c0d6f2a
Jason Li <jason.li@kitware.com>
2012-09-07 12:43:59 -0400

    EHN: Modified OSEHRAHelper.py to support timeout
    Also added support to be able to store the text output after the wait like pexpect module
    
    Modified GlobalImport and RoutineImport to use the new interface.
    
    Also make sure that log is written to disk in case of the interrupt
    
    Depending on the IO speed of the machine, the process of importing
    globals can timeout in some cases. Here we make this a configurable value in CMake.
    
    Change-Id: I5b3582f39906de161bfbd61a88c89401576a375b

--- Python/OSEHRAHelper.py
+++ Python/OSEHRAHelper.py
@@ -29,35 +29,42 @@ except ImportError, no_pexpect:
 connection=False
 log =False
 
+DEFAULT_TIMEOUT = 60 # system wise default timeout value is 30 seconds
 #---------------------------------------------------------------------------
 class PROMPT(object):
   """Wait for a VISTA> prompt in current namespace."""
 
 class ConnectMUMPS(object):
-
+  def __init__(self):
+    self.timeout = DEFAULT_TIMEOUT
   def ZN(self,namespace):
     self.wait('>')
     self.write('ZN "' + namespace + '"')
     self.namespace=namespace
     self.prompt=self.namespace + '>'
-
+  def settimeout(self, timeout):
+    self.timeout = timeout
   def login(self,username,password):
     self.wait('Username:')
     self.write(username)
     self.wait('Password')
     self.write(password)
+  def getMatch(self):
+    return None
+  def getBefore(self):
+    return None
 
   def getenv(self,volume):
     self.write('D GETENV^%ZOSV W Y')
     if sys.platform=='win32':
-      match=connection.expect([volume + ':[0-9A-Za-z-]+'],None)
+      match=connection.expect([volume + ':[0-9A-Za-z-]+'],self.timeout)
       test=match[1].span()
       VistAboxvol=''
       for i in range(test[0],test[1]):
         VistAboxvol = VistAboxvol + match[2][i]
       self.boxvol=VistAboxvol
     else:
-      connection.expect([volume + ':[0-9A-Za-z-]+'],None)
+      connection.expect([volume + ':[0-9A-Za-z-]+'])
       print connection.after
       self.boxvol=connection.after
 
@@ -78,7 +85,7 @@ class ConnectMUMPS(object):
     self.wait('DEVICE')
     if sys.platform=='win32':
       self.write('\r')
-      match=connection.expect(['\r\n[0-9]+'],None)
+      match=connection.expect(['\r\n[0-9]+'],self.timeout)
       test=match[1].span()
       number=''
       for i in range(test[0],test[1]):
@@ -87,7 +94,7 @@ class ConnectMUMPS(object):
       self.IENumber = number
     else:
       self.write('')
-      connection.expect(['\n[0-9]+'],None)
+      connection.expect(['\n[0-9]+'])
       number=connection.after
       number=number.lstrip('\r\n')
       self.IENumber = number
@@ -104,24 +111,45 @@ class ConnectWinCache(ConnectMUMPS):
     self.prompt=self.namespace + '>'
     log=file(logfile,'w')
     self.type='cache'
+    self.timeout = DEFAULT_TIMEOUT
 
-  def write(self,command ):
+  def write(self,command):
     global connection
     connection.write(command + '\r')
+    log.write(command + '\r')
+    log.flush()
 
-  def wait(self,command ):
+  def wait(self,command,timeout=None):
     global connection
     if command is PROMPT:
       command = self.prompt
-    log.write(connection.read_until(command))
-
-  def multiwait(self,options):
+    if not timeout: timeout = self.timeout
+    output = connection.expect([command],timeout)
+    self.match = output[1]
+    self.before = output[2]
+    if output[2]:
+      log.write(output[2])
+      log.flush()
+    if output[0] == -1 and output[1] == None:
+      raise Exception("Timed out")
+  def multiwait(self,options, timeout=None):
     if isinstance(options,list):
-      index=connection.expect(options)
-      log.write(index[2])
+      if not timeout: timeout = self.timeout
+      index=connection.expect(options,timeout)
+      self.match = index[1]
+      self.before = index[2]
+      if index[2]:
+        log.write(index[2])
+        log.flush()
+      if index[0] == -1 and index[1] == None:
+        raise Exception("Timed out")
       return index[0]
     else:
       raise IndexError('Input to multiwait function is not a list')
+  def getMatch(self):
+    return self.match
+  def getBefore(self):
+    return self.before
 
 class ConnectLinuxCache(ConnectMUMPS):
   def __init__(self,logfile,instance,namespace,location='127.0.0.1'):
@@ -134,22 +162,29 @@ class ConnectLinuxCache(ConnectMUMPS):
     self.prompt=self.namespace + '>'
     connection.logfile_read = file(logfile,'w')
     self.type='cache'
+    connection.timeout = DEFAULT_TIMEOUT
 
   def write(self,command ):
     connection.send(command + '\r')
 
-  def wait(self,command ):
+  def wait(self,command,timeout=None):
     global connection
     if command is PROMPT:
       command = self.prompt
-    connection.expect(command)
+    if not timeout: timeout = -1
+    connection.expect(command, timeout)
 
-  def multiwait(self,options):
+  def multiwait(self,options,timeout=None):
     if isinstance(options,list):
-      index=connection.expect(options)
+      if not timeout: timeout = -1
+      index=connection.expect(options, timeout)
       return index
     else:
       raise IndexError('Input to multiwait function is not a list')
+  def getMatch(self):
+    return connection.match
+  def getBefore(self):
+    return connection.before
 
 class ConnectLinuxGTM(ConnectMUMPS):
   def __init__(self,logfile,instance,namespace,location='127.0.0.1'):
@@ -162,22 +197,29 @@ class ConnectLinuxGTM(ConnectMUMPS):
           self.prompt="GTM>"
     connection.logfile_read = file(logfile,'w')
     self.type='GTM'
+    connection.timeout = DEFAULT_TIMEOUT
 
   def write(self,command ):
     connection.send(command + '\r')
 
-  def wait(self,command ):
+  def wait(self,command,timeout=None):
     global connection
     if command is PROMPT:
       command = self.prompt
-    connection.expect(command)
+    if not timeout: timeout = -1
+    connection.expect(command, timeout)
 
-  def multiwait(self,options):
+  def multiwait(self,options,timeout=None):
     if isinstance(options,list):
-      index=connection.expect(options)
+      if not timeout: timeout = -1
+      index=connection.expect(options, timeout)
       return index
     else:
       raise IndexError('Input to multiwait function is not a list')
+  def getMatch(self):
+    return connection.match
+  def getBefore(self):
+    return connection.before
 
 def ConnectToMUMPS(logfile,instance='CACHE',namespace='VISTA',location='127.0.0.1'):
 
