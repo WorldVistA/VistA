821639038333bd3371c82f5f63f4f17c306784a3
Luis Ibanez <luis.ibanez@kitware.com>
2012-12-04 15:33:14 -0500

    Updated CMake from 2.8.8 to 2.8.10.

--- Scripts/installCMakeinUserAccount.sh
+++ Scripts/installCMakeinUserAccount.sh
@@ -5,9 +5,9 @@ mkdir -p $HOME/cmake
 mkdir -p $HOME/cmake/src
 mkdir -p $HOME/cmake/bin
 cd $HOME/cmake/src
-wget http://www.cmake.org/files/v2.8/cmake-2.8.8.tar.gz
-tar -xzf cmake-2.8.8.tar.gz
+wget http://www.cmake.org/files/v2.8/cmake-2.8.10.tar.gz
+tar -xzf cmake-2.8.10.tar.gz
 cd $HOME/cmake/bin
-$HOME/cmake/src/cmake-2.8.8/configure --prefix=$HOME/local
+$HOME/cmake/src/cmake-2.8.10/configure --prefix=$HOME/local
 make -j4
 make install
