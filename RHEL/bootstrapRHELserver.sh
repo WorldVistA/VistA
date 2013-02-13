97f6aeaf5827064f0022c5e71854ebda87a19145
Christopher Edwards <Christopher.Edwards@krminc.com>
2013-02-13 12:15:42 -0500

    Restructure bootstrapRHELserver like Ubuntu
    Copy/Paste bootstrapUbuntuServer.sh and change as appropriate to
    yum/RHEL conventions.

--- RHEL/bootstrapRHELserver.sh
+++ RHEL/bootstrapRHELserver.sh
@@ -1,55 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------
+# Copyright 2011-2012 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
 
-#-------------------------------------------------------------
-#
-#  This script must be run by root or with sudo permissions.
-#
-#  It starts the minimal setup to configure the RHEL server.
-#
-#  It expects as argument the name of the user account
-#  to be created.
-#
-#-------------------------------------------------------------
+# Make sure we are root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
 
-#-------------------------------------------------------------
-#
-# Install Git, so that we can bring up the rest of scripts
-#
-#-------------------------------------------------------------
-yum install git
+# Update the server from repositories
+# TODO: kill output
+# TODO: log status
+# TODO: determine if this needs to be logged
+yum update -y
 
-
-#-------------------------------------------------------------
-#
-# Create the user.
-#
-#-------------------------------------------------------------
-useradd $1
-
-
-#-------------------------------------------------------------
-#
-# Change the user's password.
-#
-#-------------------------------------------------------------
-passwd $1
-
-
-#-------------------------------------------------------------
-#
-# Add the user to the list of sudoers
-#
-#-------------------------------------------------------------
-echo "$1  ALL=(ALL)   ALL" > /etc/sudoers.d/customsudoers
-chmod 0440 /etc/sudoers.d/customsudoers
-
-
-
-#---------------------------------------------------------------
-#
-# Add the cache manager user, and add our own user to its group.
-#
-#---------------------------------------------------------------
-useradd cacheusr
-sudo usermod -a -G cacheusr $1
-groups $1
+# Install baseline packages
+# TODO: detect virtualbox and install additions?
+yum install -y git xinetd perl wget ssh expect
