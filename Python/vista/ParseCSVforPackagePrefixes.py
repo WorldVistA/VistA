8c88ecbc94fd4a18e450bb63996cdc2a1886da6c
Joseph Snyder <joe.snyder@kitware.com>
2013-03-27 17:31:22 -0400

    Python: Add PackagePrefix parsing library code
    Add a file ParseCSVforPackagePrefixes.py which contains the code to read
    a csv file like the Packages.csv and parse the Package and prefix
    information and return a sorted list of prefixes.
    
    Add a test data .csv file and a test which uses that data and compares it
    to a hardcoded value.
    
    Add a README.rst file to explain the code and give examples of output.
    
    OSEHRA-Issue: http://issues.osehra.org/browse/OAT-55
    Change-Id: Ic784b6983ab68e40bed1ba4d15078d1f2a3ee355

---
+++ Python/vista/ParseCSVforPackagePrefixes.py
@@ -0,0 +1,107 @@
+#---------------------------------------------------------------------------
+# Copyright 2013 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+import csv,os,sys
+
+class Prefix:
+  def __init__(self, exclude, name):
+    self.exclude = exclude
+    self.name = name
+  def __str__(self):
+    return self.exclude + self.name
+  def __cmp__(l,r):
+    """Order prefixes from shortest to longest, ignoring exclude mark."""
+    if len(l.name) < len(r.name):
+        return -1
+    elif len(l.name) > len(r.name):
+        return +1
+    else:
+        return cmp(l.name,r.name)
+
+def FindPackagePrefixes(packagename,packages_csv_file):
+  packages_csv = csv.DictReader(open(packages_csv_file,'rb'))
+  package_dir_name = packagename.replace('_',' ')
+  packageprefix = []
+  """
+   This section finds the package prefixes that will be used for the XINDEX testing.
+   This list is then sorted by size to be used in either the ^%RD utility (Cache) or
+   the ^%RSEL utility (GT.M)
+
+   Uncategorized:  Takes all package prefixes from the Packages.csv file and
+   changes the list of prefixes so that the package-prefixed routines will be removed
+   from the listing, and the excluded-prefixed routines are kept.  This will remove the
+   routines that we know are part of a package, leaving behind only the Uncategorized.
+   IE:
+   Clinical Information Resource Network  RG        ->     'RG
+                                          MRF       ->     'MRF
+                                          !RGED     ->     RGED
+                                          !RGUT     ->     RGUT
+                                          !RGWB     ->     RGWB
+  All other Packages:  Take the package prefixes that correspond to their respective
+  and use the prefixes without changing the inclusion boolean.  This setup will give
+  the list of routines for each individual package.
+
+  Clinical Information Resource Network RG        ->     RG
+                                        MRF       ->     MRF
+                                        !RGED     ->     'RGED
+                                        !RGUT     ->     'RGUT
+                                        !RGWB     ->     'RGWB
+  The prefixes are then sorted shortest to longest by length, and passed to the function that
+  uses the routine utility to list the proper routines.
+
+  The above prefixes will create a list like this:
+  ['RG', 'MRF', "'RGED", "'RGUT", "'RGWB"]
+  """
+  included = set()
+  excluded = set()
+  if packagename == "Uncategorized":
+    for fields in packages_csv:
+      if fields['Prefixes']:
+        prefix = fields['Prefixes']
+        if prefix.startswith("!"):
+          included.add(prefix[1:])
+        else:
+          excluded.add(prefix)
+    included = included - excluded
+  else:
+    inSpecifiedPackage=False
+    for fields in packages_csv:
+      newDirName = fields['Directory Name']
+      if newDirName:
+        if newDirName == package_dir_name:
+          inSpecifiedPackage=True
+        elif inSpecifiedPackage:
+          break
+      if inSpecifiedPackage:
+        prefix = fields['Prefixes']
+        if prefix:
+          if prefix.startswith("!"):
+            excluded.add(prefix[1:])
+          else:
+            included.add(prefix)
+    excluded = excluded - included
+
+  included_prefixes = [Prefix( "",x) for x in included]
+  excluded_prefixes = [Prefix("'",x) for x in excluded]
+  prefixes = sorted(included_prefixes+excluded_prefixes)
+  return [str(x) for x in prefixes]
+
+if __name__ == '__main__':
+  print ('sys.argv is %s' % sys.argv)
+  if len(sys.argv) <= 1:
+    print ('Need the two arguments arguments:packagename,packages_csv_file ')
+    sys.exit()
+  prefixes = FindPackagePrefixes(sys.argv[1], sys.argv[2])
+  print prefixes
---
+++ Testing/Python/PrefixParserTest.py
@@ -0,0 +1,36 @@
+#---------------------------------------------------------------------------
+# Copyright 2013 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+### set up the IO that is need by unit test
+import sys,os
+sys.path = [os.path.dirname(__file__)+'/../../Python/vista'] + sys.path
+
+print ('sys.argv is %s' % sys.argv)
+if len(sys.argv) <= 1:
+  print ('Need the two arguments arguments:packagename,packages_csv_file ')
+  sys.exit()
+
+from ParseCSVforPackagePrefixes import FindPackagePrefixes
+
+ExpectedOutput =   ["'ABC", "'BCD", "'DEF", 'ABCD', "'CDEH", 'DEFG']
+TestOutput = FindPackagePrefixes(sys.argv[1],sys.argv[2])
+
+if ExpectedOutput == TestOutput:
+  print "Output of test matches the expected output"
+  sys.exit(0)
+else:
+  print "Error:  Expected output was: " + ExpectedOutput + ".  Test output was: " + TestOutput
+  sys.exit(1)
