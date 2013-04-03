8a35dac432af3dfd4675b4bf30e7e5b1174906b6
Joseph Snyder <joe.snyder@kitware.com>
2013-04-03 09:10:02 -0400

    Problem List: Add test and revert patch check
    Add new file ZZRGUTEX to the TestList file for inclusion in the MUnit test
    
    Restore the 'Q:$T(^GMPLAPI2)=""' line back to the beginning of the file in
    the ZZRGUT4. This line causes the test not to run unless the refactoring
    patch is installed.
    
    OSEHRA-Issue: http://issues.osehra.org/browse/OAT-60
    Change-Id: I3df501fd858d70416d30a29882ad6a574d2a8c1f

--- Packages/Problem List/Testing/MUnit/ZZRGUT4.m
+++ Packages/Problem List/Testing/MUnit/ZZRGUT4.m
@@ -1,5 +1,6 @@
 ZZRGUT4 ;RGI/VSL - Unit Tests - Problem List ;4/24/12
  ;;1.0;UNIT TEST;;Apr 25, 2012;Build 1;
+ Q:$T(^GMPLAPI2)=""
  TSTART
  I $T(EN^XTMUNIT)'="" D EN^XTMUNIT("ZZRGUT4")
  TROLLBACK
