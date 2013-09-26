73b018ac6608fe195851df28a033ed2676c9926e
Christopher Edwards <Christopher.Edwards@krminc.com>
2013-09-26 10:08:32 -0400

    Remove unnecessary files
    Most of these scripts clone repositories and make configuration changes
    that are replicated in other scripts and thus unnecessary.

--- Scripts/Install/installBashConfiguration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-#  Add configuration lines to the .bashrc file.
-#
-echo "" >> $HOME/.bashrc
-echo '# OSEHRA User specific aliases and functions' >> $HOME/.bashrc
-echo 'export PATH=$PATH:$HOME/local/bin' >> $HOME/.bashrc
