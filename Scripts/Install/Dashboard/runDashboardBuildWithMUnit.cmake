35a732c8366d8f41cdb4d070cbdf02b95f23d3cb
Christopher Edwards <Christopher.Edwards@krminc.com>
2013-10-02 13:22:23 -0400

    Modify bundled dashboard cmake scripts
    change suffix to .vagrant and the default os to ubuntu.

--- Scripts/Install/Dashboard/runDashboardBuildWithMUnit.cmake
+++ Scripts/Install/Dashboard/runDashboardBuildWithMUnit.cmake
@@ -1,6 +1,6 @@
 #Client maintainer: name@email
-set(CTEST_SITE "$ENV{HOST_NAME}.rackspace")
-set(CTEST_BUILD_NAME "RHEL6.1-GT.M-MUnit")
+set(CTEST_SITE "$ENV{HOST_NAME}.vagrant")
+set(CTEST_BUILD_NAME "Ubuntu12.04-GT.M-MUnit")
 set(dashboard_cache "
 GTMPROFILE:PATH=$ENV{gtmprofilefile}
 VISTA_GLOBALS_DIR:PATH=$ENV{VistAGlobalsDir}
