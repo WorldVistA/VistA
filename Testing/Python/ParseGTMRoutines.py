156d6ebad09cdf90cfd6860bf422292133ed0776
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2013-10-03 13:55:52 -0400

    Parse gtmroutines env. variable
    Added python script that parses the gtmroutines environment variable to fetch
    possible GT.M source paths and prints it to stdout.
    CMake executes this script and picks up the stdout to provide the user with a
    list of options for the MUnit routines import directory.
    
    OSEHRA-Id: http://issues.osehra.org/browse/OAT-88
    Change-Id: Id9e8caeb34c0c16b2ab7573aca56677dfdfb0c20

---
+++ Testing/Python/ParseGTMRoutines.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#---------------------------------------------------------------------------
+# Copyright 2013 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+import os
+import re
+
+def check_dir(element, final_list):
+  #replace all "\\ " with " " as os.path cannot check escaped spaces
+  elementPath = re.subn(r'\\\s', ' ', element)[0]
+  if os.path.isdir(elementPath):
+    final_list.append(elementPath)
+
+def parse_gtmroutines():
+  var = os.getenv('gtmroutines')
+  #First, replace unescaped spaces with semicolons
+  tmp = var.replace(" ",";").replace("\;","\ ")
+  tmpl = tmp.split(";")
+  num_elements = len(tmpl)
+  final_list = []
+  for ind in xrange(num_elements):
+    element = tmpl[ind]
+    element = element.strip(")")
+    paren_check = [m.start() for m in re.finditer("\(",element)]
+    if not paren_check:
+      check_dir(element, final_list)
+    else:
+      stripElement = element[paren_check[0]+1:]
+      check_dir(stripElement, final_list)
+
+  # Remove duplicates, and print the semicolon separated string
+  final_list = list(set(final_list))
+  final_str = ';'.join(final_list)
+  print final_str
+
+if __name__ == "__main__":
+  parse_gtmroutines()
