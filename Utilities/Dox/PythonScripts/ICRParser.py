ffd392d7252898c7a52ea2654b984ea29bc122f6
Jason Li <lij@osehra.org>
2016-04-14 15:05:13 -0400

    Add keywords as a subfile, fixed a few issues
    Change-Id: I56f6155195d668f2f21755ccea92a4e55da17bcd

--- Utilities/Dox/PythonScripts/ICRParser.py
+++ Utilities/Dox/PythonScripts/ICRParser.py
@@ -3,12 +3,14 @@ import re
 import os.path
 import json
 import argparse
-
+import pprint
+from LogManager import logger, initConsoleLogging
 
 # regular  expression for fields
-START_OF_RECORD = re.compile('^NUMBER: [0-9]+ ')
-GENERIC_START_OF_RECORD = re.compile('^( +)?(?P<name>[A-Z/]+( [A-Z/#]+)?): ')
-GENERIC_FIELD_RECORD = re.compile('( )?(?P<name>[A-Z/]+( [A-Z/#]+)?): ')
+START_OF_RECORD = re.compile('^(?P<name>NUMBER): ')
+GENERIC_START_OF_RECORD = re.compile('^( +)?(?P<name>[A-Z/]+( [A-Z/#]+)*): ')
+DBA_COMMENTS = re.compile('^( +)?(?P<name>DBA Comments): ')
+GENERIC_FIELD_RECORD = re.compile('( )?(?P<name>[A-Z/]+( [A-Z/#]+)*): ')
 # This is dictories of all possible sub-files in the schema
 SUBFILE_FIELDS = {
     'GLOBAL REFERENCE': [
@@ -24,7 +26,7 @@ SUBFILE_FIELDS = {
     'VARIABLES': [
         'TYPE',
         'VARIABLES DESCRIPTION'
-    ]
+    ],
     'SUBSCRIBING PACKAGE': [
         'ISC',
         'SUBSCRIBING DETAILS'
@@ -35,7 +37,8 @@ SUBFILE_FIELDS = {
         'WITH CONCURRENCE OF'
     ],
     'EDITOR': [
-        'DATE/TIME EDITED'
+    ],
+    'KEYWORDS': [
     ]
 }
 
@@ -44,42 +47,134 @@ class ICRParser(object):
         self._totalRecord = 0 # total number of record
         self._curRecord = None # current record object
         self._outObject = [] # store output result
-        self._curSubFile = []
-    def parse(self, filename):
-        with open(filename,'r') as ICRFile:
+        self._curLineNo = 0
+        self._curField = None
+        self._curStack = []
+    def parse(self, inputFilename, outputFilename):
+        with open(inputFilename,'r') as ICRFile:
             for line in ICRFile:
-                match = START_OF_RECORD.search(line):
+                line = line.rstrip("\r\n")
+                self._curLineNo +=1
+                if len(line.strip()) == 0:
+                    continue
+                match = START_OF_RECORD.match(line)
                 if match:
                     self._startOfNewItem(match, line)
                     continue
                 match = GENERIC_START_OF_RECORD.search(line)
+                if not match:
+                    match = DBA_COMMENTS.match(line)
                 if match:
                     fieldName = match.group('name')
+                    self._curField = fieldName
                     if self._isSubFile(fieldName):
-                        self._startOfSubFile()
-                    if startm:
-                        print startm.group('name');
-                        newstart = line[startm.end():]
-                        #print "start of seaching line from: ", newstart
-                        for result in GENERIC_FIELD_RECORD.finditer(newstart):
-                            print result.group('name');
-
+                        self._startOfSubFile(match, line)
+                    else:
+                        # figure out where to store the record
+                        self._rewindStack();
+                        self._findKeyValueInLine(match, line, self._curRecord)
+                elif self._curField and self._curField in self._curRecord:
+                    if not (type(self._curRecord[self._curField]) is list):
+                        preVal = self._curRecord[self._curField]
+                        self._curRecord[self._curField] = []
+                        self._curRecord[self._curField].append(preVal)
+                    self._curRecord[self._curField].append(line)
+                else:
+                    if self._curRecord:
+                        print 'No field associated with line %s: %s ' % (self._curLineNo, line)
+        logger.debug('End of file now')
+        if len(self._curStack) > 0:
+            self._curField = None
+            self._rewindStack()
+            logger.debug('Add last record: %s', self._curRecord)
+            self._outObject.append(self._curRecord)
+        # pprint.pprint(self._outObject);
+        with open(outputFilename, 'w') as out_file:
+            json.dump(self._outObject,out_file, indent=4)
     def _isSubFile(self, field):
         return field in SUBFILE_FIELDS
     def _isSubFileField(self, subFile, field):
-        return self._isSubFile(subFile) && field in SUBFILE_FIELDS[subFile]
+        return self._isSubFile(subFile) and field in SUBFILE_FIELDS[subFile]
     def _startOfNewItem(self, matchObj, line):
+        logger.debug('Starting of new item: %s', self._curStack)
+        self._curField = None
+
+        self._rewindStack()
         if self._curRecord:
             self._outObject.append(self._curRecord)
-        self.curRecord = {}
-    def _startOfSubFile(self, subFile, line):
+        self._curRecord = {}
+        self._findKeyValueInLine(matchObj, line, self._curRecord)
+        #pprint.pprint(self._curRecord)
+    def _findKeyValueInLine(self, match, line, outObj):
+        """ parse all name value pair in a line and put back in outObj"""
+        name = match.group('name'); # this is the name part
+        # now find if there is any other name value pair in the same line
+        restOfLine = line[match.end():]
+        allmatches = []
+        for m in GENERIC_FIELD_RECORD.finditer(restOfLine):
+            allmatches.append(m);
+        if allmatches:
+            for idx, rm in enumerate(allmatches):
+                if idx == 0:
+                    outObj[name] = restOfLine[:rm.start()].strip()
+                if idx == len(allmatches) -1 :
+                    outObj[rm.group('name')] = restOfLine[rm.end():].strip()
+                else:
+                    outObj[allmatches[idx-1].group('name')] = restOfLine[allmatches[idx-1].end():rm.start()].strip()
+        else:
+            outObj[name] = line[match.end():].strip()
 
-    def _findKeyValue
-    def _startOfSubFile(self, )
+    def _startOfSubFile(self, match, line):
+        """
+            for start of the sub file, we need to add a list element to the current record if it not there
+            reset _curRecord to be a new one, and push old one into the stack
+        """
+        subFile = match.group('name')
+        logger.debug('Start parsing subFile: %s, %s', subFile, line)
+        while len(self._curStack) > 0: # we are in subfile mode
+            prevSubFile = self._curStack[-1][1]
+            if prevSubFile == subFile: # just continue with more of the same subfile
+                self._curStack[-1][0].setdefault(subFile, []).append(self._curRecord) # append the previous result
+                logger.debug('append previous record the current stack')
+                break;
+            else: # this is a different subfile # now check if it is a nested subfile
+                if self._isSubFileField(prevSubFile, subFile): # this is a nested subFile, push to stack
+                    logger.debug('Nested subFile, push to the stack')
+                    self._curStack.append((self._curRecord, subFile))
+                    logger.debug('Nested subFile, stack is %s', self._curStack)
+                    break;
+                else: # this is a different subFile now:
+                    logger.debug('different subFile')
+                    preStack = self._curStack.pop()
+                    logger.debug('Pop stack')
+                    preStack[0].setdefault(preStack[1], []).append(self._curRecord)
+                    self._curRecord = preStack[0]
+                    logger.debug('different subFile, stack is %s', self._curStack)
+        if len(self._curStack) == 0:
+            self._curStack.append((self._curRecord, subFile)) # push a tuple, the first is the record, the second is the subFile field
+            logger.debug('push to stack: %s', self._curStack)
+        self._curRecord = {}
+        self._findKeyValueInLine(match, line, self._curRecord)
+    def _rewindStack(self):
+        logger.debug('rewindStack is called')
+        while len(self._curStack) > 0: # we are in subFile Mode
+            if not self._isSubFileField(self._curStack[-1][1], self._curField):
+                preStack = self._curStack.pop()
+                logger.debug('pop previous stack item: %s', preStack)
+                preStack[0].setdefault(preStack[1],[]).append(self._curRecord)
+                logger.debug('reset current record: %s', preStack)
+                self._curRecord = preStack[0]
+            else:
+                logger.debug('in subFile Fields: %s, record: %s', self._curField, self._curRecord)
+                break
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='VistA ICR File Parser')
     parser.add_argument('icrfile', help='path to the VistA ICR file')
+    parser.add_argument('outJson', help='path to the output JSON file')
     result = parser.parse_args()
+    initConsoleLogging()
     if result.icrfile:
         icrParser = ICRParser()
-        icrParser.parse(result.icrfile)
+        icrParser.parse(result.icrfile, result.outJson)
