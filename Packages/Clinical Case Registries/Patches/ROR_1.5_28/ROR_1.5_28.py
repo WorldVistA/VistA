4337d5e049ce1365f3f4f0bf2339f57c799d13ca
Joseph Snyder <joe.snyder@kitware.com>
2016-11-02 14:23:13 -0400

    Add August 2016 through October 2016 patches
    Modifications made to available patches:
    Removed from IB_DG_BUNDLE_1_0.KIDs due to lack of DSIV patches on FOIA site (lines 983-986)
      "BLD",9504,"REQB",7,0)
      DSIV*2.2*12^2
      "BLD",9504,"REQB","B","DSIV*2.2*12",7)
    
    Changed patch designation for HMP from HMP*2*0 to HMP 2.0 in
    HMP-2_SEQ-1_PAT-1.TXT:
      Associated patches: (v)HMP 2.0    <<= must be installed BEFORE
      `HMP*2*1'
    
    Change-Id: I76b80b76238690c7dddcd15861fea1d9b85bba3d

---
+++ Packages/Clinical Case Registries/Patches/ROR_1.5_28/ROR_1.5_28.py
@@ -0,0 +1,63 @@
+#---------------------------------------------------------------------------
+# Copyright 2016 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+from DefaultKIDSBuildInstaller import DefaultKIDSBuildInstaller
+
+""" This is an example of custom installer to handle post install questions
+    Requirement for custom installer python script:
+    1. Must be a class named CustomInstaller
+    2. The constructor __init__ takes the exact arguments as the
+       DefaultKIDSBuildInstaller
+    3. Preferred to be a subclass of DefaultKIDSBuildInstaller
+    4. Refer to DefaultKIDSBuildInstaller for methods to override
+    5. If not a subclass of DefaultKIDSBuildInstaller, must have a method
+       named runInstallation, and take an argument connection
+       from VistATestClient.
+"""
+class CustomInstaller(DefaultKIDSBuildInstaller):
+  def __init__(self, kidsFile, kidsInstallName,
+               seqNo = None, logFile = None, multiBuildList=None,
+               duz=17, **kargs):
+    print kidsInstallName, seqNo
+    assert kidsInstallName == "ROR*1.5*28" and int(seqNo) == 28
+    DefaultKIDSBuildInstaller.__init__(self, kidsFile,
+                                       kidsInstallName,
+                                       seqNo, logFile,
+                                       multiBuildList,
+                                       duz, **kargs)
+  """
+    @override DefaultKIDSBuildInstaller.__handleKIDSInstallQuestions__
+  """
+  def __handleKIDSInstallQuestions__(self, connection, connection2=None):
+    connection.send("Install\r")
+    connection.expect("Select INSTALL NAME:")
+    connection.send(self._kidsInstallName+"\r")
+    """ handle any questions before general KIDS installation questions"""
+    result = self.handleKIDSInstallQuestions(connection)
+    if not result:
+      return False
+    kidsMenuActionLst = self.KIDS_MENU_OPTION_ACTION_LIST
+    kidsMenuActionLst.append(("Maximum number of registry update subtasks", "", False))
+    kidsMenuActionLst.append(("Suspend the post-install during the peak hours", "", False))
+    kidsMenuActionLst.append(("Date/Time to run the new registry initialize task", "", False))
+    while True:
+      index = connection.expect([x[0] for x in kidsMenuActionLst])
+      sendCmd = kidsMenuActionLst[index][1]
+      if sendCmd != None:
+        connection.send("%s\r" % sendCmd)
+      if kidsMenuActionLst[index][2]:
+        break
+    return True
