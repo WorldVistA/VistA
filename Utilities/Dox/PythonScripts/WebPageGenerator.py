8441954ed0c27cd12065ae9b69d8f054d1b60e34
Joseph Snyder <joe.snyder@kitware.com>
2017-02-27 11:33:45 -0500

    Add "Required Field" notation to Dox display
    Add a check which will find the "_isRequired" attribute of the FileMan
    Fields that is set by the data parser and add a message to the details of
    the field that denotes the field as a required field.
    
    OSHERA-Id: http://issues.osehra.org/browse/VIVIAN-193
    Change-Id: Icf0c59ff6b834edfc3433d3c8dbfa272817de788

--- Utilities/Dox/PythonScripts/WebPageGenerator.py
+++ Utilities/Dox/PythonScripts/WebPageGenerator.py
@@ -827,13 +827,15 @@ class WebPageGenerator:
             fieldNoTxt = "<a name=\"%s\">%s</a>" % (fieldNo, fieldNo)
             fieldRow = [fieldNoTxt, value.getName(), location, value.getTypeName()]
             fieldDetails = ""
+            if value.__getattribute__("_isRequired"):
+                fieldDetails += "<div style='text-align: center'><b>************************REQUIRED FIELD************************</b></div>"
             if value.isSetType():
                 # nice display of set members
                 setIter = value.getSetMembers()
-                fieldDetails = generateHtmlListData(setIter)
+                fieldDetails += generateHtmlListData(setIter)
             elif value.isFilePointerType():
                 if value.getPointedToFile():
-                    fieldDetails = (getFileManFileHypeLink(value.getPointedToFile()))
+                    fieldDetails += (getFileManFileHypeLink(value.getPointedToFile()))
             elif value.isVariablePointerType():
                 fileManFiles = value.getPointedToFiles()
                 for pointedToFile in fileManFiles:
@@ -841,7 +843,7 @@ class WebPageGenerator:
             elif value.isSubFilePointerType():
                 filePointer = value.getPointedToSubFile()
                 if filePointer:
-                    fieldDetails = getFileManSubFileHypeLinkByName(filePointer.getFileNo())
+                    fieldDetails += getFileManSubFileHypeLinkByName(filePointer.getFileNo())
             # logic to append field extra details here
             fieldDetails += self.__generateFileManFieldPropsDetailsList__(value)
             fieldRow.append(fieldDetails)
