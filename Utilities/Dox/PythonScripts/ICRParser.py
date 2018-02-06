cd0531e68c191da53bd13b84e6e9b69f876a2416
Betsy McPhail <betsy.mcphail@kitware.com>
2018-02-06 15:44:51 -0500

    The 'Generated from...' text on ICR pages is not correct
    For example:
    'Generated from C:/Users/joe.snyder/Desktop/2017 August,03 IA Listing Descriptions'
    
    Shoule be:
    'Generated from August 03, 2017 IA Listing Descriptions'
    
    ViViaN issue: https://issues.osehra.org/browse/VIVIAN-309
    
    Change-Id: Ic4056ca5e1f649af74848975bcf3fcead3537e0b

--- Utilities/Dox/PythonScripts/ICRParser.py
+++ Utilities/Dox/PythonScripts/ICRParser.py
@@ -15,15 +15,15 @@ def run(args):
     args.date = ICRFileToJson.date
     if args.date is None:
         # No date specified in the file, try to parse the filename
-        # Expected format: <month>_<day>_<year>_IA_Listing_Descriptions.TXT
-        filename = args.icrfile
+        # Expected format: <year>_<month>_<day>_IA_Listing_Descriptions.TXT
+        filename = os.path.basename(args.icrfile)
         s = filename.find("_IA_Listing_Descriptions")
         if s > 0:
             date_str = filename[:s]
             vals = date_str.split("_")
             if len(vals) == 3:
-                args.date = vals[0] + " " + vals[1] + "," + vals[2]
-
+                # Format: <month> <day>, <year>
+                args.date = vals[1] + " " + vals[2] + ", " + vals[0]
     if args.html:
       ICRJsonToHtml.run(args)
 
