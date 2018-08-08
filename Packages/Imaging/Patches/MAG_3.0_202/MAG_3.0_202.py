ea38743353d71b10a2644aba85ec8ed21e9cad9c
Joseph Snyder <joe.snyder@kitware.com>
2018-08-08 16:47:28 -0400

    Add FOIA patches from May to August 2018
    The following patches were removed due to lack of required patches:
    
    PRCA*4.5*:
      315, 318, 321, 337, 339
    
    YS*5.01*:
      121, 129
    
    PRCA_IB_EPAYMENTS_BUNDLE_2_0
    
    Change-Id: I87a937087bc39d90b8e964560c451cf0d6ab4b76

---
+++ Packages/Imaging/Patches/MAG_3.0_202/MAG_3.0_202.py
@@ -0,0 +1,52 @@
+#---------------------------------------------------------------------------
+# Copyright 2016 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+from DefaultKIDSBuildInstaller import DefaultKIDSBuildInstaller
+from VistAMenuUtil import VistAMenuUtil
+import re
+
+inputRegEx = re.compile("INPUT TO WHAT FILE", re.I)
+
+""" This is an example of custom installer to handle post install questions
+    Requirement for custom installer python script:
+    1. Must be a class named CustomInstaller
+    2. The constructor __init__ takes the exact arguments as the
+       DefaultKIDSBuildInstaller
+    3. Preferred to be a subclass of DefaultKIDSBuildInstaller
+    4. Refer to DefaultKIDSBuildInstaller for methods to override
+    5. If not a subclass of DefaultKIDSBuildInstaller, must have a method
+       named runInstallation, and take an argument connection
+       from VistATestClient.
+"""
+class CustomInstaller(DefaultKIDSBuildInstaller):
+  def __init__(self, kidsFile, kidsInstallName,
+               seqNo = None, logFile = None, multiBuildList=None,
+               duz=17, **kargs):
+    print kidsInstallName, seqNo
+    assert kidsInstallName == "MAG*3.0*202"
+    DefaultKIDSBuildInstaller.__init__(self, kidsFile,
+                                       kidsInstallName,
+                                       seqNo, logFile,
+                                       multiBuildList,
+                                       duz, **kargs)
+  """
+    @override DefaultKIDSBuildInstaller.__gotoKIDSMainMenu__
+    to  stop Taskman and to prevent logons so that the VA FileMan patch can be installed
+  """
+
+  def runPreInstallationRoutine(self, connection, **kargs):
+    connection.expect("Update Imaging Index Terms with the latest Distribution")
+    connection.send("\r")
