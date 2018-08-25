6d0f5cb4d53f61b98366cbfa06fef6fc7880ab2a
Betsy McPhail <betsy.mcphail@kitware.com>
2018-08-25 10:23:14 -0400

    Add ICR Parser tests
    Ensure ICR scripts can be run independently from command line and use
    ICRParser.py to drive all ICR generation possibilities:
    1. Just JSON (DOX)
    2. JSON and HTML (DOX and/or ViViaN  - need JSON to create HTML)
    3. JSON and PDF (DOX) and HTML (ViViaN)
    4. JSON and PDF (DOX)
    
    Also:
    * Rename pkgMap to PACKAGE_MAP
    * Remove unused file
    * Normalize Windows paths if given as CMake arguments
    
    See https://issues.osehra.org/browse/VIVIAN-375
    
    Note: Test must be run by hand from the command line, see
    https://issues.osehra.org/browse/VIVIAN-378 to add to nightly tests.
    
    Change-Id: I2046c6cd961a93e293a30a788ffa7da34d06d3b2

--- Utilities/Dox/PythonScripts/ICRSchema.py
+++ Utilities/Dox/PythonScripts/ICRSchema.py
@@ -1,6 +1,25 @@
-"""
-This file defined the reverse engineered ICR fileman schema.
-"""
+# This file defined the reverse engineered ICR fileman schema
+#---------------------------------------------------------------------------
+# Copyright 2018 The Open Source Electronic Health Record Alliance
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+import re
+import os
+
+RPC_FILE_NO = '8994'
+RPC_NAME_FIELD_NO = '.01'
 
 """ This is a list of keywords in ICR top level files
     This list is a sequence order of each fields
@@ -123,6 +142,10 @@ DATE_TIME_FIELD = set([
     'DEFERRED UNTIL'
 ])
 
+# regular  expression for fields
+INTEGRATION_REFERENCES_LIST = re.compile('^[\r\f]?INTEGRATION REFERENCES LIST *(.*)(([01]\d|2[0-3]):([0-5]\d)|24:00) *PAGE')
+
+
 """ SOME UTILITY FUNCTIONS  """
 def isSubFile(field):
     return field in SUBFILE_FIELDS
@@ -133,5 +156,21 @@ def isSubFileField(subFile, field):
 def isWordProcessingField(field):
     return field in WORDS_FIELDS
 
-def isDateTimeField(field):
-    return field in DATE_TIME_FIELD
+def getDate(icrFilename):
+    with open(icrFilename,'r') as ICRFile:
+        for line in ICRFile:
+            line = line.rstrip("\r\n")
+            match = INTEGRATION_REFERENCES_LIST.match(line)
+            if match:
+                return match.group(1).strip()
+    # No date specified in the file, try to parse the filename
+    # Expected format: <year>_<month>_<day>_IA_Listing_Descriptions.TXT
+    filename = os.path.basename(icrFilename)
+    s = filename.find("_IA_Listing_Descriptions")
+    if s > 0:
+        date_str = filename[:s]
+        vals = date_str.split("_")
+        if len(vals) == 3:
+            # Format: <month> <day>, <year>
+            return vals[1] + " " + vals[2] + ", " + vals[0]
+    return None
