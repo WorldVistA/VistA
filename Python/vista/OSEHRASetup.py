11bf5bab949e0beb513cb57e6519a65a29bafc23
Joseph Snyder <joe.snyder@kitware.com>
2019-04-12 15:12:47 -0400

    Remove Resource Logging
    Add the setup code to turn off resource logging during the course
    of running the PostImportSetupScript.
    
    Change-Id: If8240c6169c07db896e8326194049613770de7a9

--- Python/vista/OSEHRASetup.py
+++ Python/vista/OSEHRASetup.py
@@ -140,6 +140,27 @@ def setupPrimaryHFSDir(VistA,hfs_dir):
   VistA.wait('Select OPTION:')
   VistA.write('')
 
+def removeResourceUsageLogging(VistA):
+  # By default, prevent logging of Resource Usage
+  startFileman(VistA)
+  VistA.write('1')
+  VistA.wait_re('INPUT TO WHAT FILE')
+  VistA.write('KERNEL SYSTEM PARAMETERS')
+  VistA.wait('EDIT WHICH FIELD')
+  VistA.write('LOG RESOURCE USAGE')
+  VistA.wait('THEN EDIT FIELD')
+  VistA.write('')
+  VistA.wait('DOMAIN NAME')
+  # `1 is the notation to grab the entry with a number of 1
+  VistA.write('`1')
+  VistA.wait('LOG RESOURCE USAGE')
+  VistA.write('NO')
+  VistA.wait('DOMAIN NAME')
+  VistA.write('')
+  VistA.wait('Select OPTION:')
+  VistA.write('')
+
+
 def setupIntroText(VistA, introTextSHA):
   # Set up the introduction text for the VistA system
   #
